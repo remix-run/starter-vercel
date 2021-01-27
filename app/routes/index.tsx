a68493fcc124980a0c5865c870dace84aa683a73
Ryan Florence <rpflorence@gmail.com>
2021-01-27 13:58:36 -0700

    updated to 0.10.0

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,23 +2,22 @@ import { useRouteData } from "@remix-run/react";
 import type { Loader } from "@remix-run/data";
 
 export let loader: Loader = async () => {
-  return {
-    message: "this is awesome 😎",
-  };
+  return { message: "this is awesome 😎" };
 };
 
-export let meta = () => {
+export function meta() {
   return {
     title: "Remix Starter",
     description: "Welcome to remix!",
   };
-};
+}
 
 export default function Index() {
   let data = useRouteData();
 
   return (
-    <div style={{ padding: "20px 0" }}>
+    <div style={{ textAlign: "center", padding: 20 }}>
+      <h2>Welcome to Remix!</h2>
       <p>
         <a href="https://remix.run/dashboard/docs">Check out the docs</a> to get
         started.
